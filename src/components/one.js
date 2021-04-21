ce564cd75df86ea30fc54b0240c4504a0aaa37c2
samudrala aravind <samudralaaravind1708@gmail.com>
2021-04-21 13:11:00 +0530

    sjjsja

---
+++ src/components/one.js
@@ -0,0 +1,75 @@
+import React,{Component} from "react"
+
+import "./one.css"
+import {BrowserRouter as Router,Switch,Link,Route} from "react-router-dom"
+export class  Test extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name:"Arvaind",
+            age:20,
+            count:0
+        };
+    }
+    shot(){
+        this.setState((prev,props)=>({
+            count:prev.count+1
+        }));
+    }
+    render(){
+        return (
+            <div>
+                <center>My name is : {this.state.name}</center>
+                <center>My age is: {this.state.age}</center>
+                
+                <button onClick={this.shot}>Shot</button>
+            </div>
+        );
+    }
+}
+export function App(){
+    var first = {
+        color:"blue",
+        backgroundColor:"red",
+        padding:"10px",
+        border:"2px solid tomato"
+    };
+    return (
+        <Router>
+            <div>
+                <nav style = {first}>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/users">Users</Link>
+                    </li>
+                </nav>
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/users">
+                         <Users />
+                    </Route>
+                    <Route path="/">
+                         <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+function Home(){
+    return <h1>This is Home page</h1>;
+}
+function Users(){
+    return <h3>This is Users page</h3>;
+}
+function About(){
+    return <h2>This is ABout page</h2>;
+}
+
---
+++ src/index.js
@@ -0,0 +1,14 @@
+//git push -u  origin main --> is used to push code to github
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App,Test} from "./components/one"
+
+ReactDOM.render(
+  <div>
+    <App />
+    <Test />
+  </div>,
+  document.getElementById('root')
+);
+
+
